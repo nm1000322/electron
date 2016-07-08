09cba36d94a3cb539267eef7b9791f207cf581ce
nqmetke <nqmetke@gmail.com>
2016-07-07 23:48:20 -0400

    I need sleep or somthing
    Probably

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const remote = require('electron').remote;
 const {webFrame} = require('electron');
 const main = remote.require('./index.js');
-function buttonPress() {
-  main.displayText();
-}
+$('.image').css('box-shadow', '10px 10px 5px #888');
