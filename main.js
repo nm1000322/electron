135ff83ff2ddddd6ac42d539f0e7ab410127e255
nqmetke <nqmetke@gmail.com>
2016-07-08 09:41:05 -0400

    Yep
    adding a few things

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 const remote = require('electron').remote;
 const {webFrame} = require('electron');
 const main = remote.require('./index.js');
-$('.image').css('box-shadow', '10px 10px 5px #888');
+
+var lightToggle = 1;
+
+$(".lumos").click(function(){
+  if (lightToggle == 1){
+  $("html").css("background-color", "black");
+  lightToggle = 0;
+  console.log(lightToggle);
+}
+else if (lightToggle == 0) {
+  $("html").css("background-color", "white");
+  lightToggle = 1;
+}
+});
+
+image.addEventListener('touchstart', function(){
+  $(".image").css("box-shadow", "10px 20px 30px black");
+}, false)
+$(".mainImage").click(function(){
+  $(".mainimage").css("box-shadow", "10px 20px 30px black");
+});
